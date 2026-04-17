ed424355a0c45bc3ec103b27883fa196811bab54
Michael Carpiano <michael.carpiano@smus.ca>
2026-04-17 08:50:18 -0700

    Add methods for storing questions and sessions

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Server starting...");
         Database.getConnection();
+
+        //Demo "add question"
+        Database.insertQuestion(
+                "math",
+                3,
+                """
+                {
+                  "question": "What is 2+2?",
+                  "answers": [3, 4, 5],
+                  "correct": 4
+                }
+                """
+        );
     }
 }
